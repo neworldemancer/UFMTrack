986d7addf5dac636389c618b6bf0000728024b1d
Mykhailo Vladymyrov <neworldemancer@gmail.com>
2023-09-11 14:55:39 +0200

    add license. correct processing time start offset

--- GUFMTrack.py
+++ GUFMTrack.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # coding: utf-8
+"""
+   Copyright 2015-2023, University of Bern, Laboratory for High Energy Physics and Theodor Kocher Institute, M. Vladymyrov
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+   
+   This module performs tracking of cells under flow based on cells segmentation.
+"""
+
 from __future__ import annotations
 
 
@@ -318,11 +336,11 @@ class Cell:
     """
     Class representing one other_cell or a group of under-segmented cells
     """
-    _dt = cfgm.DT
-
-    _dt_offset = cfgm.DT_OFFSET
 
     def __init__(self, in_line: str):
+        self._dt = cfgm.DT
+        self._dt_offset = cfgm.DT_OFFSET
+
         arr = in_line.split()
 
         # t idx M nM [list m-idxs], x, y, z, w, phi, ecc, [aux-channels]
--- classificator.py
+++ classificator.py
@@ -1,3 +1,21 @@
+"""
+   Copyright 2015-2023, University of Bern, Laboratory for High Energy Physics and Theodor Kocher Institute, M. Vladymyrov
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+Module for classification of cell vs not-a-cell and detaching vs transmigrating cells based on cell features.
+"""
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import pickle
